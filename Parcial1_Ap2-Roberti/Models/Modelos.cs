399286456ce047b47aad1671912fc49cc6049f0f
Roberti Amarante <yavier.ag@gmail.com>
2017-06-21 18:44:27 -0400

    Agregando Paquetes

---
+++ Parcial1_Ap2-Roberti/Models/Modelos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2-Roberti.Models
+{
+    public class Modelos
+    {
+    }
+}
